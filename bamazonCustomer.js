b116ba350df926b57742ed1b389fc628bba961fa
Dangggchris <christophermtdang@gmail.com>
2019-06-11 20:10:02 -0700

    added initial prompt function

---
+++ bamazonCustomer.js
@@ -0,0 +1,64 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var keys = require("./keys.js");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: keys.serverPW,
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    firstPrompt();
+  });
+
+function firstPrompt() {
+    
+        inquirer
+          .prompt([{
+            name: "id",
+            type: "input",
+            message: "What is product ID?",
+            validate: function(value) {
+              if (isNaN(value) === false) {
+                return true;
+              }
+              return false;
+            }
+          },
+          {
+              name: "count",
+              type: "input",
+              message: "How many do you want?",
+              validate: function(value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+              }
+          }])
+          .then(function(answer) {
+            var query = "SELECT * FROM products WHERE ?";
+                connection.query(query, {item_id: answer.id}, function (error,response){
+                    console.log(response);
+                })
+          });
+}
+
+function allItems() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+        console.log(res);
+        }
+        console.log("-----------------------------------");
+    });
+}
